ac0997509299cb6f4fc6503c6bf5dac4bdb60ef3
Vule028 <52829208+Vule028@users.noreply.github.com>
2021-12-14 18:06:13 +0100

    Add files via upload

---
+++ [system]/esxbalkan_chat/srw.lua
@@ -0,0 +1,20 @@
+AddEventHandler('chatMessage', function(Source, Name, Msg)
+    args = stringsplit(Msg, " ")
+    CancelEvent()
+    if string.find(args[1], "/") then
+        local cmd = args[1]
+        table.remove(args, 1)  
+    end
+end)
+
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
