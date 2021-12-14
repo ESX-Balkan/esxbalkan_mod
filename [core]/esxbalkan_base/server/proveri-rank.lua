78000dccde268ca29fa197601d4aab7afde1cf59
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-12-14 21:12:53 +0100

    Update proveri-rank.lua

--- [core]/esxbalkan_base/server/proveri-rank.lua
+++ [core]/esxbalkan_base/server/proveri-rank.lua
@@ -5,10 +5,10 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterServerCallback("esx:proveriRank", function(source, cb)
     local player = ESX.GetPlayerFromId(source)
 
-    if player ~= nil then
+    if player then
         local playerGroup = player.getGroup()
 
-        if playerGroup ~= nil then 
+        if playerGroup then 
             cb(playerGroup)
         else
             cb("user")
