1fb1aa3bcb5a31a39a20723d766bc791122a4f9b
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-12-14 20:55:18 +0100

    Update config.lua

--- [!esx-bitno]/esxbalkan_status/config.lua
+++ [!esx-bitno]/esxbalkan_status/config.lua
@@ -2,5 +2,5 @@ Config = {}
 
 Config.StatusMax      = 1000000
 Config.TickTime       = 1000
-Config.UpdateInterval = 30000
+Config.UpdateInterval = 60000 -- Bolje 60000 radi server side optimizacije
 Config.Display        = false -- Enable the esx_status bars (disable if you are using another HUD)
