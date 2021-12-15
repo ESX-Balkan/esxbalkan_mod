0da3fc1955d8223f2f5ba38295352be2b3e9d9f4
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-12-15 12:38:28 +0100

    Update richpresence.lua

--- [core]/esxbalkan_base/server/richpresence.lua
+++ [core]/esxbalkan_base/server/richpresence.lua
@@ -1,1 +1,11 @@
+GlobalState["BrojIgraca"] = 0
+
+Citizen.CreateThread(function()
+    while true do
+        local igraci = GetPlayers()
+        GlobalState["BrojIgraca"] = #igraci
+        Citizen.Wait(5000)
+    end
+end)
+
 GlobalState.brojSlotova = GetConvar("sv_maxclients", "8")
