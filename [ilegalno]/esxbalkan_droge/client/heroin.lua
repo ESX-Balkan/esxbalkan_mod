6b13617a6ce384f52ebc29cec9271a99a606f87f
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-12-15 12:23:14 +0100

    Update heroin.lua

--- [ilegalno]/esxbalkan_droge/client/heroin.lua
+++ [ilegalno]/esxbalkan_droge/client/heroin.lua
@@ -21,7 +21,7 @@ end)
 Citizen.CreateThread(function()
 	while true do
 
-		Citizen.Wait(0)
+		Citizen.Wait(5)
 
 		local playerPed = PlayerPedId()
 		local coords = GetEntityCoords(playerPed)
@@ -204,7 +204,9 @@ CreateThread(function()
     while true do
         Wait(0)
         if isPickingUp then
-            DisableAllControlActions(0);
+            DisableAllControlActions(0)
+	else
+	    Wait(500)
         end
     end
 end)
