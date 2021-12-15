13a920aee4d969fd9c083951c744bd2eaff014c3
Vule028 <52829208+Vule028@users.noreply.github.com>
2021-12-15 17:02:35 +0100

    Add files via upload

---
+++ [voice-telefoni]/pma-voice/fxmanifest.lua
@@ -0,0 +1,34 @@
+game {'gta5'}
+
+fx_version 'cerulean'
+author 'AvarianKnight'
+description 'VOIP built using FiveM\'s built in mumble.'
+
+lua54 'yes'
+
+shared_script 'shared.lua'
+
+client_scripts {
+    'client/**/*.lua',
+}
+
+server_scripts {
+    'server/**/*.lua'
+}
+
+files {
+    'ui/*.ogg',
+    'ui/css/*.css',
+    'ui/js/*.js',
+    'ui/index.html',
+}
+
+ui_page 'ui/index.html'
+
+provides {
+	'mumble-voip',
+    -- why does it use so many different names
+    'tokovoip',
+    'toko-voip',
+    'tokovoip_script'
+}
