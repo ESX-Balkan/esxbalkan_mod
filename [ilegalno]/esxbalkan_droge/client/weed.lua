d38098fc6cedaf77e5cbc99cc800f4a7733b7eba
Vule028 <52829208+Vule028@users.noreply.github.com>
2021-12-17 00:15:14 +0100

    Update weed.lua

--- [ilegalno]/esxbalkan_droge/client/weed.lua
+++ [ilegalno]/esxbalkan_droge/client/weed.lua
@@ -30,17 +30,7 @@ Citizen.CreateThread(function()
 
 			if IsControlJustReleased(0, Keys['E']) and not isProcessing then
 
-				if Config.LicenseEnable then
-					ESX.TriggerServerCallback('esx_license:checkLicense', function(hasProcessingLicense)
-						if hasProcessingLicense then
-							ProcessWeed()
-						else
-							OpenBuyLicenseMenu('weed_processing')
-						end
-					end, GetPlayerServerId(PlayerId()), 'weed_processing')
-				else
-					ProcessWeed()
-				end
+				ProcessWeed()
 
 			end
 		else
