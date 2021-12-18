55de059df692b2dce346b41eafdbf15ee2f20a8e
Sefik17 <78441706+Sefik17@users.noreply.github.com>
2021-12-18 20:51:48 +0100

    Nije radio richpresence

--- [core]/esxbalkan_base/client/richpresence.lua
+++ [core]/esxbalkan_base/client/richpresence.lua
@@ -1,13 +1,13 @@
 Citizen.CreateThread(function()
 	while true do
-		SetDiscordAppId(908862004184358942)
-		SetDiscordRichPresenceAsset('esx-balkan-logo')
+	SetDiscordAppId(921851620906643567)
+	SetDiscordRichPresenceAsset('esx-balkan-logo')
         SetDiscordRichPresenceAssetText('by ESX BALKAN TEAM')
         SetDiscordRichPresenceAssetSmall('esx-balkan-logo')
         SetDiscordRichPresenceAssetSmallText('by ESX BALKAN TEAM')
         SetDiscordRichPresenceAction(0, "Nas Discord!", "https://discord.gg/7SK3fGDeHN")
-        SetDiscordRichPresenceAction(1, "Connectaj se!", "fivem://connect/178.33.34.113:30120")
+        SetDiscordRichPresenceAction(1, "Connectaj se!", "fivem://connect/ip:30120")
         SetRichPresence("ID: " .. GetPlayerServerId(PlayerId()) .. " | " .. GlobalState["BrojIgraca"] .. "/" .. GlobalState.brojSlotova)    
-		Citizen.Wait(60000)
+	Citizen.Wait(60000)
 	end
 end)
