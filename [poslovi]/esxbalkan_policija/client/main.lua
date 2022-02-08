b4d809d0aade14ad65cd25ef9eaa8eb5755a58ad
Vule028 <52829208+Vule028@users.noreply.github.com>
2022-02-08 10:23:53 +0100

    Update main.lua
    Fixano za nestajanje markera posle reloga

--- [poslovi]/esxbalkan_policija/client/main.lua
+++ [poslovi]/esxbalkan_policija/client/main.lua
@@ -8,6 +8,23 @@ if ESX.PlayerLoaded and ESX.PlayerData.job == 'police' then
 	TriggerServerEvent('esx_policejob:forceBlip')
 end
 
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerData = xPlayer
+	ESX.PlayerLoaded = true
+end)
+
+RegisterNetEvent('esx:onPlayerLogout')
+AddEventHandler('esx:onPlayerLogout', function()
+	ESX.PlayerLoaded = false
+	ESX.PlayerData = {}
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+end)
+
 function cleanPlayer(playerPed)
 	SetPedArmour(playerPed, 0)
 	ClearPedBloodDamage(playerPed)
